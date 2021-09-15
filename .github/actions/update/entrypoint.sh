7a49fc7cf7d775444d0ea3fe97882d75f8a8e2ff
Morten Lied Johansen <morten.lied.johansen@nav.no>
2021-09-15 19:31:11 +0200

    Set up workflow to update metadata on push

--- .github/actions/update/entrypoint.sh
+++ .github/actions/update/entrypoint.sh
@@ -4,16 +4,7 @@ set -o errexit   # abort on nonzero exitstatus
 set -o nounset   # abort on unbound variable
 set -o pipefail  # pipeline returns last non-zero status
 
-eval "$(ssh-agent -s)"
-ssh-add - <<< "${REPO_KEY}"
-cd "$(mktemp --directory)"
-git clone git@github.com:nais/nais-ppa.git
-cd nais-ppa
-
 # 'Unpack' deb file for PPA purposes
-sudo apt update
-sudo apt install dpkg-dev apt-utils
-wget ${URL} -O ${BASENAME}
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 apt-ftparchive release . > Release
@@ -27,13 +18,12 @@ echo RELOADAGENT | gpg-connect-agent
 gpg --batch --import <(echo -n "${GPG_PRIVATE_KEY}" | base64 -d)
 
 # Sign repo artifacts
-echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "${EMAIL}" -abs -o - Release > Release.gpg
-echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "${EMAIL}" --clearsign -o - Release > InRelease
+echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "aura@nav.no" -abs -o - Release > Release.gpg
+echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "aura@nav.no" --clearsign -o - Release > InRelease
 
 # Commit and push changes
-git config user.email "${EMAIL}"
-git config user.name "nais-ppa add_package action"
+git config user.email "aura@nav.no"
+git config user.name "nais-ppa update action"
 git add .
 git --no-pager diff --cached
-git commit -a -m "Add ${BASENAME}"
-git push
+git commit -a -m "Update metadata"
