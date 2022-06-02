431bae00f259db621365120d550bbb143a169b83
Morten Lied Johansen <morten.lied.johansen@nav.no>
2022-06-02 12:15:38 +0200

    Minor debugging tweaks to actions

--- .github/actions/cleanup/entrypoint.sh
+++ .github/actions/cleanup/entrypoint.sh
@@ -10,16 +10,19 @@ NUMBER_TO_KEEP=10
 echo Contents of working directory ${PWD}:
 ls -la
 
-
+echo --- Removing old files
 for name in 'naisdevice-*.deb' 'nais_*.deb'; do
   printf "%s\n" ${name} | head -n "-${NUMBER_TO_KEEP}" | xargs --no-run-if-empty --verbose git rm
 done
 
-# Commit and push changes
 if ! git diff --staged --quiet; then
+  echo --- Commit and push changes
   git config user.email "aura@nav.no"
   git config user.name "nais-ppa cleanup action"
   git add .
   git --no-pager diff --cached
   git commit -a -m "Update metadata"
+  git push
+else
+  echo --- No changes to commit
 fi
--- .github/actions/update/entrypoint.sh
+++ .github/actions/update/entrypoint.sh
@@ -9,12 +9,12 @@ echo Contents of working directory ${PWD}:
 ls -la
 
 
-# 'Unpack' deb file for PPA purposes
+echo --- 'Unpack' deb file for PPA purposes
 apt-ftparchive packages . > Packages
 gzip -k -f Packages
 apt-ftparchive release . > Release
 
-# Set-up GPG
+echo --- Set-up GPG
 mkdir -p ~/.gnupg
 chmod u=rwx,g=,o= ~/.gnupg
 echo "use-agent" >> ~/.gnupg/gpg.conf
@@ -23,13 +23,14 @@ echo "allow-loopback-pinentry" >> ~/.gnupg/gpg-agent.conf
 echo RELOADAGENT | gpg-connect-agent
 gpg --batch --import <(echo -n "${GPG_PRIVATE_KEY}" | base64 -d)
 
-# Sign repo artifacts
+echo --- Sign repo artifacts
 echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "aura@nav.no" -abs -o - Release > Release.gpg
 echo ${GPG_PASSPHRASE} | gpg --batch --no-tty --pinentry-mode=loopback --yes --passphrase-fd 0 --default-key "aura@nav.no" --clearsign -o - Release > InRelease
 
-# Commit and push changes
+echo --- Commit and push changes
 git config user.email "aura@nav.no"
 git config user.name "nais-ppa update action"
 git add .
 git --no-pager diff --cached
 git commit -a -m "Update metadata"
+git push
