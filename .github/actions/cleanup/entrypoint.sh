52dcd1f2579e20a3177cd9df77733d7e317c1a13
nais-ppa cleanup action <aura@nav.no>
2022-06-02 10:47:27 +0200

    Tweak cleanup action

--- .github/actions/cleanup/entrypoint.sh
+++ .github/actions/cleanup/entrypoint.sh
@@ -6,13 +6,20 @@ set -o pipefail  # pipeline returns last non-zero status
 
 NUMBER_TO_KEEP=10
 
+# For debugging
+echo Contents of working directory ${PWD}:
+ls -la
+
+
 for name in 'naisdevice-*.deb' 'nais_*.deb'; do
-  printf "%s\n" ${name} | head -n "-${NUMBER_TO_KEEP}" | xargs --no-run-if-empty --verbose rm
+  printf "%s\n" ${name} | head -n "-${NUMBER_TO_KEEP}" | xargs --no-run-if-empty --verbose git rm
 done
 
 # Commit and push changes
-git config user.email "aura@nav.no"
-git config user.name "nais-ppa cleanup action"
-git add .
-git --no-pager diff --cached
-git commit -a -m "Update metadata"
+if ! git diff --staged --quiet; then
+  git config user.email "aura@nav.no"
+  git config user.name "nais-ppa cleanup action"
+  git add .
+  git --no-pager diff --cached
+  git commit -a -m "Update metadata"
+fi
